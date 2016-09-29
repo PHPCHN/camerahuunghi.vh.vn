f2cf1235ae5431750d04362f9b0b6d03fc5e2bc6
PianDV <khdng0424@gmail.com>
2016-09-29 15:45:23 +0700

    setup database

---
+++ htdocs/app/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories';
+
+    /**
+     * Define attributes deleted_at of the data.
+     *
+     * @var string
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'sup_id',
+    ];
+}
